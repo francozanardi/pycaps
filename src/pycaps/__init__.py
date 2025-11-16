3410289f2d7642ad3e5e6f5c20ea3cf0dcf59257
Franco Zanardi <francozanardi.97@gmail.com>
2025-11-15 22:19:39 -0300

    chore: bump version to 0.2.0

--- src/pycaps/__init__.py
+++ src/pycaps/__init__.py
@@ -10,4 +10,4 @@ from .layout.definitions import *
 from .ai import LlmProvider
 from .template import TemplateLoader, TemplateFactory, DEFAULT_TEMPLATE_NAME
 
-__version__ = "0.1.0" 
+__version__ = "0.2.0" 
